af81fa0f89cfe04e3a3188575c8ec8a5c170526f
pk-bin <pk-bin@gmail.com>
2025-01-03 01:17:36 +0900

    로그 주석 추가

--- cute-log/src/lib.rs
+++ cute-log/src/lib.rs
@@ -1,14 +1,6 @@
 use log::LevelFilter;
 
+/// 로깅 기능을 추가. 이때 기본값으로 info 이상의 log 만 지원하도록 함.
 pub fn init_logger() {
     env_logger::builder().filter_level(LevelFilter::Info).init();
-    /*
-    let logfile = log4rs::append::file::FileAppender::builder().
-        encoder(Box::new(log4rs::encode::pattern::PatternEncoder::default())).build(log_path).unwrap();
-
-    log4rs::init_config(log4rs::config::Config::builder()
-        .appender(log4rs::config::Appender::builder().build("logfile",Box::new(logfile)))
-        .build(log4rs::config::Root::builder().appender("logfile").build(LevelFilter::Info)).unwrap()
-    ).unwrap();
-    */
 }
