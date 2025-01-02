6eecdec52aef6104fa166c7267fa7576258d1fc0
pk-bin <pk-bin@gmail.com>
2025-01-03 01:15:27 +0900

    주석 수정 및 이전 불필요한 내용 삭제. 잘못 Control C + V 내용 수정

--- cute-core/src/lib.rs
+++ cute-core/src/lib.rs
@@ -6,18 +6,16 @@ pub use self::errors::{CuteError, CuteErrorCode};
 pub type DataStream<T> = Pin<Box<dyn tokio_stream::Stream<Item = Result<T, CuteError>> + Send>>;
 
 /// # Comment
-/// 특정 작업에 대한 것을 잓성하는 Trait 이다.
+/// 작업 내용을 정의
 ///
 /// 생성자에 의해 생성될 것을 생각하여 trait 에 new 를 추가하였다.
 ///
 /// 내부 `execute` 작업시에 해당 객체에 메모리 등을 생성하여 해제 할 경우를 대비하여 destroy 도 추가함.
-/// # 보장 및 설명
-/// + Sync : 여러 Thread 간의 참조(ref)시 공유 가능하도록 합니다.
-/// + Send : 여러 Thread 간의 이동을 보장합니다.
-/// + 생존주기 : 생성자를 통해 생성된 후에 Scope 를 벗어날 경우 없어지기에 `'static` 을 보장하지 않음.
 #[async_trait::async_trait]
 pub trait Task<C> {
     /// 생성자.
+    ///
+    /// 입력받은 input 을 `serde::Deserialize` 해도 되게 안해도 되도록 구현.
     fn new(input : Option<Box<[u8]>>) -> Result<Box<dyn Task<C> + Send>, CuteError>
     where Self: Sized;
 
@@ -30,31 +28,37 @@ pub trait Task<C> {
     async fn destroy(&mut self);
 }
 
+/// # Comment
 /// Task 생성자.
 ///
-/// 이것을 만드는 이유는 Procedure 특성을 지정받을 것을 만들때 Worker 의 Output 및 Task 의 Output 을 명시적으로 정의해줘야 하는데
+/// 현재는 사라진 worker 의 역솰을 다른 곳에서 하도록 하고
 ///
-/// 그 작업이 쉽지 않기 때문. 그레서 생성 대리자와 같은 느낌으로 사용.
-pub trait TaskConstructor<C> : Send + Sync + 'static
+/// 생성자만 받도록 추가함.
+///
+/// 해당 작업은 macro 를 통해 만들기 쉽게 구현함.
+pub trait TaskConstructor<C> : Send + Sync
 where C : Send + Sync + 'static,
 {
-    /// 사용시 Task::new() 가 내부적으로 동작한다. 이것이 전부이다.
+    /// 사용시 Task::new() 를 통해 Task 를 생성한다.
+    ///
+    /// Impl 하지 않고 dynamic 하게 하여 std::collection 등에 추가하기 쉽게 하였다.
     fn create(&self, input : Option<Box<[u8]>>) -> Result<Box<dyn Task<C> + Send>, CuteError>;
 }
 
 /// # Comment
-/// 해당 Trait 은 Worker 를 통하여 특정 작업에 대한 동작을 정리한 요소들을 Collection 에 저장하여 사용하는 Trait 이다.
+/// 특정한 작업들을 재활용 하도록 hash_map 와 같은 std::collection 에 기록하여 관리
 ///
-/// 해당 Trait 특성을 받은 Struct 는 반드시 Parameter 로 제공되는 key 와 매칭되도록 해야함.
+/// 실세 작업에 대한 절차는 `get_task` 를 통해 받은 곳에서 절차를 작업함.
+///
+/// 여기서는 `TaskConstructor`에 의해 생성된 `Task` 만을 반환함.
 #[async_trait::async_trait]
 pub trait Procedure<C> {
-    /// 해당 Collection 의 모든 service ( worker ) 이름들을 반환.
+    /// 현재 std::collection 에 기록된 모든 작업 내용을 반환.
     async fn get_service_names(&self) -> Result<Vec<String>, CuteError>;
-    /// Key 를 통해 Value (Worker) 를 추출하고 추출한 Worker 에 대하여 `one_of_execute` 수행함.
+    /// 해당 이름을 가진 작업을 반환.
     async fn get_task(&self, key : Box<str>, input : Option<Box<[u8]>>) -> Result<Box<dyn Task<C> + Send>, CuteError>;
 }
 
 mod serdes;
-
 mod procs;
 mod errors;
